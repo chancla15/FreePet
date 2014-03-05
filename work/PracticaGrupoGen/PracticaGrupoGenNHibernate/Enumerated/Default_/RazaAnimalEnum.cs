dde26ea632d6c2df3064814a3c203e46f924900b
Rodrigo Martn de Lamo <mcgod69esp@gmail.com>
2014-03-05 18:02:29 +0100

    Intento de borrado version antigua

--- work/PracticaGrupoGen/PracticaGrupoGenNHibernate/Enumerated/Default_/RazaAnimalEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace PracticaGrupoGenNHibernate.Enumerated.Default_
-{
-public enum RazaAnimalEnum { Perro=1, Gato=2, Pajaro=3, Pez=4, Reptil=5 };
-}
--- work/PracticaGrupoGen/PracticaGrupoGenNHibernate/Enumerated/Default_/TamanyoAnimalEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace PracticaGrupoGenNHibernate.Enumerated.Default_
-{
-public enum TamanyoAnimalEnum { Pequeño=1, PequeñoMedio=2, Normal=3, NormalGrande=4, Grande=5 };
-}
