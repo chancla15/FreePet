2679370740d16943109a36dfc3640901c7a07435
chancla15 <jm91.osx@gmail.com>
2014-04-07 13:32:26 +0200

    Todo borrado fallan los .bin, .exe, .suo etc

--- GestionVeterinariaGen/GestionVeterinariaGenNHibernate/Enumerated/GestionVeterinaria/DosisEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace GestionVeterinariaGenNHibernate.Enumerated.GestionVeterinaria
-{
-public enum DosisEnum { Uno_al_dia=1, Dos_al_dia=2, Tres_al_dia=3 };
-}
--- GestionVeterinariaGen/GestionVeterinariaGenNHibernate/Enumerated/GestionVeterinaria/SexoEnum.cs
+++
@@ -1,7 +0,0 @@
-
-using System;
-
-namespace GestionVeterinariaGenNHibernate.Enumerated.GestionVeterinaria
-{
-public enum SexoEnum { Macho=1, Hembra=2 };
-}
