c4f64069592c3d827cffe122edc34cf05db13115
chancla15 <jm91.osx@gmail.com>
2014-05-18 16:32:31 +0200

    Modificacion diseño de login

--- GestionVeterinariaGen/WindowsFormsApplication1/FormLogin.Designer.cs
+++ GestionVeterinariaGen/WindowsFormsApplication1/FormLogin.Designer.cs
@@ -35,25 +35,24 @@
             this.btn_salir = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_accpt
             // 
+            this.btn_accpt.BackColor = System.Drawing.Color.Gray;
             this.btn_accpt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_accpt.Location = new System.Drawing.Point(134, 106);
+            this.btn_accpt.Location = new System.Drawing.Point(309, 187);
             this.btn_accpt.Name = "btn_accpt";
             this.btn_accpt.Size = new System.Drawing.Size(90, 38);
             this.btn_accpt.TabIndex = 0;
             this.btn_accpt.Text = "ACEPTAR";
-            this.btn_accpt.UseVisualStyleBackColor = true;
+            this.btn_accpt.UseVisualStyleBackColor = false;
             this.btn_accpt.Click += new System.EventHandler(this.btn_accept_Click);
             // 
             // text_user
             // 
             this.text_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text_user.Location = new System.Drawing.Point(135, 18);
+            this.text_user.Location = new System.Drawing.Point(201, 113);
             this.text_user.Name = "text_user";
             this.text_user.Size = new System.Drawing.Size(198, 22);
             this.text_user.TabIndex = 1;
@@ -61,7 +60,7 @@
             // text_pass
             // 
             this.text_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text_pass.Location = new System.Drawing.Point(135, 49);
+            this.text_pass.Location = new System.Drawing.Point(201, 145);
             this.text_pass.Name = "text_pass";
             this.text_pass.Size = new System.Drawing.Size(198, 22);
             this.text_pass.TabIndex = 2;
@@ -73,31 +72,34 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(131, 83);
+            this.label1.Location = new System.Drawing.Point(35, 187);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.Size = new System.Drawing.Size(41, 16);
             this.label1.TabIndex = 3;
+            this.label1.Text = "error";
+            this.label1.Visible = false;
             // 
             // btn_salir
             // 
+            this.btn_salir.BackColor = System.Drawing.Color.Gray;
             this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_salir.Location = new System.Drawing.Point(231, 106);
+            this.btn_salir.Location = new System.Drawing.Point(201, 187);
             this.btn_salir.Name = "btn_salir";
             this.btn_salir.Size = new System.Drawing.Size(102, 38);
             this.btn_salir.TabIndex = 4;
             this.btn_salir.Text = "SALIR";
-            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.UseVisualStyleBackColor = false;
             this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(16, 23);
+            this.label2.Location = new System.Drawing.Point(35, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.Size = new System.Drawing.Size(88, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "USUARIO:";
             // 
@@ -105,28 +107,13 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(16, 55);
+            this.label3.Location = new System.Drawing.Point(35, 151);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.Size = new System.Drawing.Size(126, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "CONTRASEÑA:";
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.btn_salir);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.text_pass);
-            this.panel1.Controls.Add(this.text_user);
-            this.panel1.Controls.Add(this.btn_accpt);
-            this.panel1.Location = new System.Drawing.Point(77, 100);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(347, 160);
-            this.panel1.TabIndex = 7;
             // 
             // FormLogin
             // 
@@ -134,9 +121,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.FONDO_APP;
-            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.ClientSize = new System.Drawing.Size(411, 237);
             this.ControlBox = false;
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_accpt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_user);
+            this.Controls.Add(this.text_pass);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -144,9 +137,8 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.FormLogin_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -159,7 +151,6 @@
         public System.Windows.Forms.Button btn_salir;
         public System.Windows.Forms.Label label2;
         public System.Windows.Forms.Label label3;
-        public System.Windows.Forms.Panel panel1;
     }
 }
 
